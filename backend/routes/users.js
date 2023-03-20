eee9b0901dbeaec4c54efdf3e2ee6aff03fc2325
YounesEch <114940175+YounesEch@users.noreply.github.com>
2023-03-20 23:12:14 +0100

    skapa user test färdig

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -18,6 +18,10 @@ router.get('/', function(req, res, next) {
 /***********skapa ny användare**********/
 router.post('/add', function(req, res, next) {  
   let newUser = {name: req.body.name};
+  let usersPassword = crypto.SHA3(req.body.password).toString();
+  let usersEmail = (req.body.email);
+  newUser.password =usersPassword;
+  newUser.email = usersEmail;
   console.log("new User", newUser);
 
   req.app.locals.db.collection("users").insertOne(newUser)
@@ -25,6 +29,11 @@ router.post('/add', function(req, res, next) {
     console.log("result från db", result);
     res.json(result);
   })
+})
+
+/***************logga in användare**********/
+router.post('/login', function(req, res, next){
+
 })
 
 
